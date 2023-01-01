ed84207811e0d8aa0c4d4bd801e3cd55bc1db0b1
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-01 16:50:39 +0330

    first commit

---
+++ src/main/java/com/example/conditionalbean/ConditionalBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.conditionalbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalBeanApplication.class, args);
+	}
+
+}
